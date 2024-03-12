5568a33cc4fa182eafc541ff6892cd7a7bb6d069
Sayira Elittzi Quirama <sequirama@soy.sena.edu.co>
2024-03-12 13:29:06 -0500

    Initial commit

---
+++ src/main/java/com/example/Bootcamp_2024_OnClass/Bootcamp2024OnClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bootcamp_2024_OnClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp2024OnClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp2024OnClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Bootcamp_2024_OnClass/Bootcamp2024OnClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bootcamp_2024_OnClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp2024OnClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
