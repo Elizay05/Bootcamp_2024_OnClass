ca4ad5edf1dc64a91042fde420bb32409d9fdb53
Sayira Elittzi Quirama <sequirama@soy.sena.edu.co>
2024-03-28 12:25:39 -0500

    List Capacities 100%

--- src/main/java/com/example/bootcamp2024onclass/adapters/driving/http/controller/CapacityRestControllerAdapter.java
+++ src/main/java/com/example/bootcamp2024onclass/adapters/driving/http/controller/CapacityRestControllerAdapter.java
@@ -9,12 +9,10 @@ import com.example.bootcamp2024onclass.domain.model.Capacity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/capacity")
@@ -31,5 +29,16 @@ public class CapacityRestControllerAdapter {
         capacity = capacityServicePort.saveCapacity(capacity);
         CapacityResponse response = capacityResponseMapper.toCapacityResponse(capacity);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<CapacityResponse>> getAllCapacities(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(required = false, defaultValue = "true") boolean isOrderByName,
+            @RequestParam(required = false, defaultValue = "true") boolean isAscending){
+        return ResponseEntity.ok(capacityResponseMapper.toCapacityResponseList(
+                capacityServicePort.getAllCapacities(page, size, isOrderByName, isAscending)
+        ));
     }
 }
