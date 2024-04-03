3e522d416d3fb9781baecab094479d9d0f541972
Sayira Elittzi Quirama <sequirama@soy.sena.edu.co>
2024-04-02 20:23:30 -0500

    List Technologies 100%

--- src/test/java/com/example/bootcamp2024onclass/configuration/BeanConfigurationTest.java
+++ src/test/java/com/example/bootcamp2024onclass/configuration/BeanConfigurationTest.java
@@ -1,8 +1,16 @@
 package com.example.bootcamp2024onclass.configuration;
 
+import com.example.bootcamp2024onclass.adapters.driven.jpa.mysql.mapper.IBootcampEntityMapper;
+import com.example.bootcamp2024onclass.adapters.driven.jpa.mysql.mapper.ICapacityEntityMapper;
 import com.example.bootcamp2024onclass.adapters.driven.jpa.mysql.mapper.ITechnologyEntityMapper;
+import com.example.bootcamp2024onclass.adapters.driven.jpa.mysql.repository.IBootcampRepository;
+import com.example.bootcamp2024onclass.adapters.driven.jpa.mysql.repository.ICapacityRepository;
 import com.example.bootcamp2024onclass.adapters.driven.jpa.mysql.repository.ITechnologyRepository;
+import com.example.bootcamp2024onclass.domain.api.IBootcampServicePort;
+import com.example.bootcamp2024onclass.domain.api.ICapacityServicePort;
 import com.example.bootcamp2024onclass.domain.api.ITechnologyServicePort;
+import com.example.bootcamp2024onclass.domain.spi.IBootcampPersistencePort;
+import com.example.bootcamp2024onclass.domain.spi.ICapacityPersistencePort;
 import com.example.bootcamp2024onclass.domain.spi.ITechnologyPersistencePort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,15 +25,41 @@ class BeanConfigurationTest {
     @MockBean
     private ITechnologyEntityMapper technologyEntityMapper;
 
+    @MockBean
+    private ICapacityRepository capacityRepository;
+
+    @MockBean
+    private ICapacityEntityMapper capacityEntityMapper;
+
+
+    @MockBean
+    private IBootcampRepository bootcampRepository;
+
+    @MockBean
+    private IBootcampEntityMapper bootcampEntityMapper;
+
     @Test
     @DisplayName("When_BeanConfiguration_Expect_PortsToBeCreatedSuccessfully")
     void testBeanConfiguration() {
-        BeanConfiguration beanConfiguration = new BeanConfiguration(technologyRepository, technologyEntityMapper);
+        BeanConfiguration beanConfiguration = new BeanConfiguration(
+                technologyRepository, technologyEntityMapper, capacityRepository, capacityEntityMapper, bootcampRepository, bootcampEntityMapper);
 
-        ITechnologyPersistencePort persistencePort = beanConfiguration.technologyPersistencePort();
-        assertNotNull(persistencePort);
+        ITechnologyPersistencePort technologyPersistencePort = beanConfiguration.technologyPersistencePort();
+        assertNotNull(technologyPersistencePort);
 
-        ITechnologyServicePort servicePort = beanConfiguration.technologyServicePort();
-        assertNotNull(servicePort);
+        ITechnologyServicePort technologyServicePort = beanConfiguration.technologyServicePort();
+        assertNotNull(technologyServicePort);
+
+        ICapacityPersistencePort capacityPersistencePort = beanConfiguration.capacityPersistencePort();
+        assertNotNull(capacityPersistencePort);
+
+        ICapacityServicePort capacityServicePort = beanConfiguration.capacityServicePort();
+        assertNotNull(capacityServicePort);
+
+        IBootcampPersistencePort bootcampPersistencePort = beanConfiguration.bootcampPersistencePort();
+        assertNotNull(bootcampPersistencePort);
+
+        IBootcampServicePort bootcampServicePort = beanConfiguration.bootcampServicePort();
+        assertNotNull(bootcampServicePort);
     }
 }
